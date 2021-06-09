710aba6ba23bc8a253d06a82d80be7de37bb8633
CyberTech-webDevelopment <cybertech.armenia21@gmail.com>
2021-06-09 17:20:06 +0300

    account_bar  lang

---
+++ resources/lang/am/account_bar.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'bar'=> [
+       'edit_profile' => 'Փոխել պրոֆիլը',
+       'buy' => 'Գնել Artstore-ում',
+       'sell' => 'Վաճառել Artstore-ում',
+       'basket' => 'Զամբյուզ',
+       'favorites' => 'Նախընտրելի',
+       'messages' => 'Նամակներ',
+       'last_views' => 'Վերջին դիտումներ',
+       'purchases' => 'Գնումներ',
+       'my_comments'=> 'Իմ մեկնաբանություններ',
+       'account_settings' => 'Կարգավորումներ',
+       'add_new_product' => 'Ավելացնել նոր ապրանք',
+       'products' => 'Ապրանքներ',
+       'wallet'=>'Դրամապանակ',
+       'sign_out' => 'Ելք',
+    ]
+];
+
+
+
